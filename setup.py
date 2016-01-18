5bfb628c5150d5105ea0faeb079f730086d93a89
hsercanatli <hsercanatli@gmail.com>
2016-01-18 13:03:37 +0200

    setup.py file for installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='makamadaptivetuning',
+      version='0.1',
+      description='A Synthesizer and an adaptive tuner for SymbTr files',
+      author='Hasan Sercan Atli',
+      url='https://github.com/hsercanatli/adaptive-tuning',
+      packages=['makamadaptivetuning'], requires=['numpy']
+      )
