e48a5f5be1053412ee1e1e84cc0729242a9c4232
hsercanatli <hsercanatli@gmail.com>
2016-01-18 12:43:40 +0200

    synth wrapper for SymbTr

---
+++ adaptive_tuning/synth_symbtr.py
@@ -0,0 +1,8 @@
+from synthesizer import *
+from musicxml_reader import read_music_xml
+
+def synth_symbtr(musicxml_path):
+    score = read_music_xml(musicxml_path)
+
+    synth_karplus(score, fn=musicxml_path[:-4] + "--karplus.wav")
+    synth_sine(score, fn=musicxml_path[:-4] + "--sine.wav")
