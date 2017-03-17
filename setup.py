a128be35d6b58b4df11c654ab6defa3498117ed3
hsercanatli <hsercanatli@gmail.com>
2017-03-17 16:42:15 +0100

    changed website and added numpy as requirement

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='symbtrsynthesis',
       version='1.0.0-dev',
       description='An (adaptive) synthesizer for SymbTr-MusicXML scores',
       author='Hasan Sercan Atli',
-      url='https://github.com/hsercanatli/adaptive-tuning',
+      url='https://github.com/hsercanatli/symbtrsynthesis',
       packages=['symbtrsynthesis'],
-      include_package_data=True,
+      include_package_data=True, install_requires=['numpy']
       )
