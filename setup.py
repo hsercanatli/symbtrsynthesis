1f5283399bb8dd12f4c309df7ac30ba6d6efb92f
Alastair Porter <alastair.porter@upf.edu>
2018-10-02 15:37:54 +0200

    Use find_packages in setup.py to include `synthesizer` module

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 # !/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='symbtrsynthesis',
       version='1.0.1-dev',
       description='An (adaptive) synthesizer for SymbTr-MusicXML scores',
       author='Hasan Sercan Atli',
       url='https://github.com/hsercanatli/symbtrsynthesis',
-      packages=['symbtrsynthesis'],
+      packages=find_packages(),
       include_package_data=True, install_requires=['numpy']
       )
